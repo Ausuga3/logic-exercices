4998aa51de1bdb021145983e772f46af0618885f
Andres Usuga <ausuga@gmail.com>
2024-12-02 22:45:08 -0500

    ejercicio 1.2

--- exercise1.py
+++ exercise1.py
@@ -15,13 +15,15 @@ def registrar_cliente():
                 "dinero":dinero}
         return cliente
         
-def comprar_producto(producto):
-    for i in producto:
-        print(i)
-    compra=input("Que deseas comprar?: ")
-    if compra in producto and compra["camisa"]["stock"]>0:
-        compra["camisa"]["stock"]=compra["camisa"]["stock"]-1
-    return producto    
+def comprar_producto(productos,cliente):
+    
+    producto=input("Que deseas comprar?: ")
+    if producto in productos and productos[producto]["stock"] > 0:
+        if cliente["dinero"] >= productos[producto]["valor"]:
+            cliente["dinero"] -= productos[producto]["valor"]
+            productos[producto]["stock"] -= 1
+
+
 
 def devolver_prodcuto():
     pass
@@ -33,26 +35,29 @@ def mostrar_estado_cliente():
     pass
         
 
+productos ={ 
+    "camisa":{"valor":5000,"stock":10,"defectuoso":0},
+    "pantalon":{"valor":7000,"stock":5,"defectuoso":0},
+    "traje":{"valor":10000,"stock":82,"defectuoso":0}
+    }
+    
 if __name__ == "__main__":
     clientes=[]   
-    productos = [
-    {"camisa":{"valor":5000,"stock":10,"defectuoso":0}},
-    {"pantalon":{"valor":7000,"stock":5,"defectuoso":0}},
-    {"traje":{"valor":10000,"stock":82,"defectuoso":0}},
-]     
 
     while True:
         opc = int(input("""Que deseas hacer:
                         1.registrarse
                         2.comprar
                         3.devolver producto
-                        4.salir"""))  
-        if opc ==1:
+                        4.salir
+                        : """))  
+        if opc == 1:
             clientes.append(registrar_cliente() )    
             print(clientes)
-        elif opc ==2:
-            compra = comprar_producto(productos)
-            print(compra)
+        elif opc == 2:
+            comprar_producto(productos,clientes)
+            print(productos,clientes)
+            
         elif opc ==3:
             pass
         elif opc == 4:
